77fd4a8d2b1ce12da2ba3c41feaddd4a3eb168a9
Josh Mills <jmills@covermymeds.com>
2017-05-18 10:53:28 -0400

    Better spaces

--- server.rb
+++ server.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'faker'
 
 post '/' do
-	{
-		version: "1.0",
-		response: {
-			outputSpeech: {
-				type: "PlainText",
-				text: "#{Faker::Hacker.say_something_smart}"
-			}
-		}
-	}.to_json
+  {
+    version: "1.0",
+    response: {
+      outputSpeech: {
+        type: "PlainText",
+        text: "#{Faker::Hacker.say_something_smart}"
+      }
+    }
+  }.to_json
 end
